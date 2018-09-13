c2ce7f72db665439c3c65a07b4d496b70cb39271
filipposantonocito <33153524+filipposantonocito@users.noreply.github.com>
2018-09-13 11:30:02 +0200

    junk: cpp

--- src/test_mqtt.cpp
+++ src/test_mqtt.cpp
@@ -6,21 +6,25 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-#include "MQTTClient.h"
+#include <iostream>
+#include <cstdlib>
+#include <map>
+#include <vector>
+#include <cstring>
+#include "mqtt/client.h"
 
 //#define ADDRESS "192.168.1.209"	// Broker IP
 #define ADDRESS "127.0.0.1"
 #define CLIENTID "ODOM ID"
 #define TOPIC "odom"
 #define QOS 2
+#define PAYLOAD "CIAO!"
 
 double roll = 0;
 double pitch = 0;
 double yaw = 0;
 
-char px[8];
-
-/*
+/* func
 void zedCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
  // roll (x-axis rotation)
@@ -43,75 +47,55 @@ void zedCallback(const nav_msgs::Odometry::ConstPtr& msg)
   double siny = +2.0 * (msg->pose.pose.orientation.w * msg->pose.pose.orientation.z + msg->pose.pose.orientation.x * msg->pose.pose.orientation.y);
   double cosy = +1.0 - 2.0 * (msg->pose.pose.orientation.y * msg->pose.pose.orientation.y + msg->pose.pose.orientation.z * msg->pose.pose.orientation.z);
   yaw = atan2(siny, cosy);
-/**/
 
-/*
  ROS_INFO("\nPosition x: [%f]\n Position y: [%f]\n Position z: [%f]\n Roll: [%lf]\n Pitch: [%lf]\n Yaw: [%lf]\n", msg->pose.pose.position.x,
                                                             msg->pose.pose.position.y,
                                                              msg->pose.pose.position.z,
                                                              roll, pitch, yaw);
-/**/
-
-/*
-  int rc = 0;
-  MQTTClient zed;
-  MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-  MQTTClient_create(&zed, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-
-  if(rc = MQTTClient_connect(zed, &conn_opts) != MQTTCLIENT_SUCCESS)
-  {
-    printf("Connection failed. Return code: %d\n", rc);
-  }
-
-  MQTTClient_message posx = MQTTClient_message_initializer;
-  sprintf(px, "%lf", msg->pose.pose.position.x);
-  posx.payload = px;
-  if(MQTTClient_publishMessage(zed, TOPIC, &posx, NULL))
-  {
-    ROS_INFO("MESSAGE PUBLISHED!\n");
-    printf("Message published\n");
-  }
 }
-/**/
+/* func */
 
 int main(int argc, char **argv)
 {
-  int rc = 0;
-  MQTTClient zed;
-  MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-  MQTTClient_create(&zed, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-
-  if(rc = MQTTClient_connect(zed, &conn_opts) != MQTTCLIENT_SUCCESS)
-  {
-    printf("Connection failed. Return code: %d\n", rc);
-  }
-
-  MQTTClient_message test = MQTTClient_message_initializer;
-  test.payload = "hi!";
-  if(MQTTClient_publishMesssage(zed, TOPIC, &test, NULL))
-  {
-    printf("MESSAGE PUBLISHED!");
-  }
+  sample_mem_persistence persist;
+  mqtt::client client(ADDRESS, CLIENTID, &persist);
+  mqtt::connect_options connOpts;
   
- /*
-  MQTTClient_message posx = MQTTClient_message_initializer;
-  sprintf(px, "%lf", msg->pose.pose.position.x);
-  posx.payload = px;
-  if(MQTTClient_publishMessage(zed, TOPIC, &posx, NULL))
+  try 
   {
-    ROS_INFO("MESSAGE PUBLISHED!\n");
-    printf("Message published\n");
+		  std::cout << "\nConnecting..." << std::endl;
+		  client.connect(connOpts);
+		  std::cout << "...OK" << std::endl;
+
+		  // First use a message pointer.
+
+		  std::cout << "\nSending message..." << std::endl;
+		  auto posx = mqtt::make_message(TOPIC, PAYLOAD1);
+		  posx->set_qos(QOS);
+		  client.publish(posx);
+		  std::cout << "...OK" << std::endl;		  
   }
- /**/
  
- /*
+	 catch (const mqtt::persistence_exception& exc) 
+  {
+		  std::cerr << "Persistence Error: " << exc.what() << " ["
+		    << exc.get_reason_code() << "]" << std::endl;
+		  return 1;
+	 }
+ 
+	 catch (const mqtt::exception& exc) 
+  {
+		  std::cerr << exc.what() << std::endl;
+		  return 1;
+	 }
+
+/*
   ros::init(argc, argv, "zedmqtt");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("/zed/odom", 1000, zedCallback);
-
+  
   ros::spin();
 /**/
  
   return 0;
 }
-
